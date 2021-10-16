0ffd0d6f2c334a47a05948ed04f286fd913ac68c
affancodex <92643499+affancodex@users.noreply.github.com>
2021-10-16 21:04:51 +0400

    Add files via upload
    Tasky-project

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+const task_Container=document.querySelector(".task_Container");
+console.log("Task Ct works")
+
+
+
+const saveChanges =()=>{
+
+const taskdata ={
+
+    id:`${Date.now()}`,
+    imageUrl:document.getElementById("imageUrl").value,
+    taskTitle:document.getElementById("taskTitle"),
+    taskType:document.getElementById("taskType"),
+    taskDescription:document.getElementById("taskDescription")
+
+};
+
+
+
+const newCard=
+`
+
+
+`
+
+
+
+
+
+};
+
